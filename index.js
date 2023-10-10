ceb77b59cdc0b9304da50362b111f1570603f4f2
tabetommy <tabe_e@yahoo.ca>
2023-10-10 15:20:41 +0200

    added personalinfo endpoint

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express=require('express');
+const bodyParser = require('body-parser');
 
 //instatiations
 const app = express();
 
+//middelware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const port = 5000;
 
+// app.get('/', (req, res) => {        
+//     res.sendFile('index.html', {root: __dirname});                                                         
+// });
+
 app.get('/', (req, res) => {        
-    res.sendFile('index.html', {root: __dirname});                                                         
+    res.send("here")                                                       
+});
+
+//contact details
+app.post('/personalinfos', (req, res) => {        
+    let data = req.body;
+    res.send('Data Received: ' + JSON.stringify(data));                                                         
 });
 
 
